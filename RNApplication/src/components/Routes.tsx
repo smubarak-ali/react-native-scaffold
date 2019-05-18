15f3965b583894d9ac1b5ea032e0f6e3122ad9a7
Mubarak <mubarakali.00@gmail.com>
2019-05-18 22:27:37 +0800

    Initial project commit

---
+++ RNApplication/src/components/Routes.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+import { View, Dimensions, StyleSheet } from "react-native";
+
+interface Props {
+
+}
+
+const Routes: FC<Props> = (props) => {
+
+    return (
+        <>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Routes;
